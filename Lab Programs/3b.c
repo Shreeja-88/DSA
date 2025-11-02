fd397f439c6c4ab875fc55e0091a7f26e84a7816
Shreeja Hebbar <itshreeja@gmail.com>
2025-11-02 13:37:11 +0530

    Modified 3b

--- Lab Programs/3b.c
+++ Lab Programs/3b.c
@@ -3,106 +3,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+// Define the node structure
+typedef struct node {
     int data;
     struct node* next;
-};
-typedef struct node NODE;
+} NODE;
 
-NODE* reverse(NODE *start);
-void display(NODE* start);
-NODE* createList(NODE* start);
+// Function to insert a node at the end
+NODE* insertEnd(NODE* start, int value) {
+    NODE* new_node = (NODE*)malloc(sizeof(NODE));
+    new_node->data = value;
+    new_node->next = NULL;
 
-int main() {
-    NODE *start = NULL;
-
-    start = createList(start);
-
-    printf("\nOriginal linked list:\n");
-    display(start);
-
-    start = reverse(start);
-
-    printf("\nReversed linked list:\n");
-    display(start);
-
-    return 0;
-}
-
-NODE* createList(NODE* start) {
-    NODE *newNode, *ptr;
-    int n, val;
-
-    printf("Enter the number of nodes: ");
-    scanf("%d", &n);
-
-    for(int i = 0; i < n; i++) {
-        printf("Enter data for node %d: ", i + 1);
-        scanf("%d", &val);
-
-        newNode = (NODE*) malloc(sizeof(NODE));
-        newNode->data = val;
-        newNode->next = NULL;
-
-        if(start == NULL)
-            start = newNode;
-        else {
-            ptr = start;
-            while(ptr->next != NULL)
-                ptr = ptr->next;
-            ptr->next = newNode;
-        }
+    if (start == NULL) {
+        return new_node;
     }
+
+    NODE* ptr = start;
+    while (ptr->next != NULL) {
+        ptr = ptr->next;
+    }
+    ptr->next = new_node;
     return start;
 }
 
-NODE* reverse(NODE *start) {
+// Function to display the list
+void display(NODE* start) {
+    NODE* ptr = start;
+    printf("Linked List: ");
+    while (ptr != NULL) {
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+// Function to reverse the linked list
+NODE* reverse(NODE* start) {
     NODE *prev, *temp, *ptr;
+
     if (start == NULL) {
         printf("List Empty\n");
         return start;
     }
-    else if (start->next == NULL) {
+
+    if (start->next == NULL) {
         printf("Only one Node %d\n", start->data);
         return start;
-    } else {
-        prev = NULL;
-        ptr = start;
-        while (ptr != NULL) {
-            temp = ptr->next;
-            ptr->next = prev;
-            prev = ptr;
-            ptr = temp;
-        }
-        start = prev;
-        return start;
     }
+    prev = NULL;
+    ptr = start;
+    while (ptr != NULL) {
+        temp = ptr->next;
+        ptr->next = prev;
+        prev = ptr;
+        ptr = temp;
+    }
+
+    start = prev;
+    return start;
 }
 
-void display(NODE* start) {
-    NODE* ptr;
-    if (start == NULL) {
-        printf("List is empty.\n");
-    } else {
-        ptr = start;
-        while (ptr != NULL) {
-            printf("%d -> ", ptr->data);
-            ptr = ptr->next;
-        }
-        printf("NULL\n");
-    }
+// Main function
+int main() {
+    NODE* start = NULL;
+
+    // Create sample list: 10 -> 20 -> 30 -> 40
+    start = insertEnd(start, 10);
+    start = insertEnd(start, 20);
+    start = insertEnd(start, 30);
+    start = insertEnd(start, 40);
+
+    printf("Original ");
+    display(start);
+
+    start = reverse(start);
+
+    printf("Reversed ");
+    display(start);
+
+    return 0;
 }
-
-
-/*
-Enter the number of nodes: 3
-Enter data for node 1: 7
-Enter data for node 2: 5
-Enter data for node 3: 6
-
-Original linked list:
-7 -> 5 -> 6 -> NULL
-
-Reversed linked list:
-6 -> 5 -> 7 -> NULL
-*/
