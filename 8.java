4daa5269db22d8293da103c87f0c7955128e9d54
Shreeja Hebbar <itshreeja@gmail.com>
2026-01-02 11:58:31 +0530

    Create 8.java

---
+++ 8.java
@@ -0,0 +1,11 @@
+// File: TestPackage.java
+import mypack.MyClass;
+
+public class TestPackage {
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.display();
+    }
+
+}
