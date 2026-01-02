7f10962eeff5a1523e9ba686eb3c0616523b6ea1
Shreeja Hebbar <itshreeja@gmail.com>
2026-01-02 11:56:34 +0530

    Create 9.java

---
+++ 9.java
@@ -0,0 +1,26 @@
+class MyThread implements Runnable {
+
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " Running: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+public class TWC {
+
+    public static void main(String args[]) {
+        MyThread m = new MyThread();
+
+        Thread t1 = new Thread(m, "Thread-1");
+        Thread t2 = new Thread(m, "Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
